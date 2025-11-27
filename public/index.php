cd4f881d5b636dc2933caeb561e6b18578af81a4
take088917761@gmail.com <yuyalikeswimming@icloud.com>
2025-11-27 09:21:51 +0900

    h()の表記、form閉じタグ、日付表示のコピペ変更漏れを修正

--- public/index.php
+++ public/index.php
@@ -68,6 +68,7 @@ try {
                   Login
         </button></a>
       </div>
+
       <!-- 今日 -->
       <div class="p-10">
         <h2 class="text-lg font-bold mb-4">today</h2>
@@ -79,13 +80,13 @@ try {
               <?= date('Y年n月j日 G時i分', strtotime($record_today['post_date'])) ?>
             </p>
             <ul class="p-0">
-              <li class="font-bold"><?= h($record_today['category'], ENT_QUOTES, 'UTF-8'); ?></li>
-              <li><?= h($record_today['study_minutes'], ENT_QUOTES, 'UTF-8'); ?>分</li>
-              <li><?= nl2br(h($record_today['comment'], ENT_QUOTES, 'UTF-8')); ?></li>
+              <li class="font-bold"><?= h($record_today['category']); ?></li>
+              <li><?= h($record_today['study_minutes']); ?>分</li>
+              <li><?= nl2br(h($record_today['comment'])); ?></li>
             </ul>
             <!-- 編集フォーム -->
             <form action="<?= BASE_URL ?>app/edit.php" method="get" class="inline-block">
-              <input type="hidden" name="id" value="<?= h($record_today['id'], ENT_QUOTES, 'UTF-8'); ?>" placeholder="input comment or comment">
+              <input type="hidden" name="id" value="<?= h($record_today['id']); ?>" placeholder="input comment or comment">
               <button type="submit" 
                       class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2">
                 編集
@@ -95,18 +96,18 @@ try {
             <!-- 削除フォーム -->
             <form action="<?= BASE_URL ?>app/delete.php" method="post" class="inline-block" 
                   onsubmit="return confirm('本当に削除しますか？');">
-              <input type="hidden" name="id" value="<?= h($record_today['id'], ENT_QUOTES, 'UTF-8'); ?>">
+              <input type="hidden" name="id" value="<?= h($record_today['id']); ?>">
               <button type="submit" 
                       class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2">
                 削除
               </button>
-            </div>
+            </form>
           </div>
-          <?php else: ?>
-            <p class="text-gray-500">今日の学習記録はありません。</p>
-          <?php endif; ?>
         </div>
-
+        <?php else: ?>
+          <p class="text-gray-500">今日の学習記録はありません。</p>
+        <?php endif; ?>
+      </div>
 
       <!-- 昨日 -->
       <div class="p-10">
@@ -118,7 +119,7 @@ try {
             <?php foreach ($records_yesterday as $yesterday) : ?>
               <div class="bg-white border border-gray-200 p-4 rounded-2xl shadow">
                 <p class="max-w-7xl mx-auto py-1 px-4 text-right text-sm text-gray-500">
-                  <?= date('Y年n月j日 G時i分', strtotime($record_today['post_date'])) ?>
+                  <?= date('Y年n月j日 G時i分', strtotime($yesterday['post_date'])) ?>
                 </p>
                 <ul p-0>
                   <li class="font-bold"><?= h($yesterday['category']); ?></li>
@@ -166,17 +167,17 @@ try {
           <?php foreach($weekly_records as $weekly_record){ ?>
           <div class="bg-white border border-gray-200 p-4 rounded-2xl shadow">
               <p class="max-w-7xl mx-auto py-1 text-right text-sm text-gray-500">
-                <?= date('Y年n月j日 G時i分', strtotime($record_today['post_date'])) ?>
+                <?= date('Y年n月j日 G時i分', strtotime($weekly_record['post_date'])) ?>
               </p>
               <ul class="mb-4 p-0">
-                <li class="font-bold"><?= h($weekly_record['category'], ENT_QUOTES, 'UTF-8'); ?></li>
-                <li><?= h($weekly_record['study_minutes'], ENT_QUOTES, 'UTF-8'); ?></li>
-                <li><?= nl2br(h($weekly_record['comment'], ENT_QUOTES, 'UTF-8')); ?></li>
+                <li class="font-bold"><?= h($weekly_record['category']); ?></li>
+                <li><?= h($weekly_record['study_minutes']); ?></li>
+                <li><?= nl2br(h($weekly_record['comment'])); ?></li>
               </ul>
 
               <!-- 編集フォーム -->
               <form action="<?= BASE_URL ?>app/edit.php" method="get" class="inline-block">
-                <input type="hidden" name="id" value="<?= h($weekly_record['id'], ENT_QUOTES, 'UTF-8'); ?>">
+                <input type="hidden" name="id" value="<?= h($weekly_record['id']); ?>">
                 <button type="submit" 
                         class="text-purple-700 hover:text-white border border-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-4 py-2">
                   編集
@@ -186,7 +187,7 @@ try {
               <!-- 削除フォーム -->
               <form action="<?= BASE_URL ?>app/delete.php" method="post" class="inline-block" 
                     onsubmit="return confirm('本当に削除しますか？');">
-                <input type="hidden" name="id" value="<?= h($weekly_record['id'], ENT_QUOTES, 'UTF-8'); ?>">
+                <input type="hidden" name="id" value="<?= h($weekly_record['id']); ?>">
                 <button type="submit" 
                         class="text-red-700 hover:text-white border border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2">
                   削除
