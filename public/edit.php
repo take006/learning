c642b316ddd6b892f7c1907643eb5c04813e9b56
take088917761@gmail.com <yuyalikeswimming@icloud.com>
2025-11-27 13:09:55 +0900

    edit.phpも学習時間記録に対応

--- public/edit.php
+++ public/edit.php
@@ -10,7 +10,7 @@ try {
   $pdo = getPDO();
   $id = (int)filter_input(INPUT_GET, "id");
   
-  $sql = "SELECT id, post_date, category, comment FROM learning_history WHERE id = :id";
+  $sql = "SELECT id, post_date, category, comment, study_minutes FROM learning_history WHERE id = :id";
   $ps = $pdo->prepare($sql);
   $ps->execute([':id' => $id]);
   $learning = $ps->fetch();
@@ -38,16 +38,20 @@ try {
     <div class="w-full md:w-2/3 lg:w-1/2 bg-white border border-gray-200 p-6 rounded-2xl shadow">
       <h2 class="text-2xl font-bold mb-6 text-center text-gray-800">編集</h2>
       <form action="<?= BASE_URL ?>app/controllers/update.php" method="post" class="flex flex-col">
-        <input type="hidden" name="id" value="<?= htmlspecialchars($learning['id'], ENT_QUOTES, 'UTF-8'); ?>">
+        <input type="hidden" name="id" value="<?= h($learning['id']); ?>">
 
         <label for="post_date" class="block mb-2 text-sm font-medium text-gray-900">日付</label>
         <input type="datetime-local" id="post_date" name="post_date"
-          value="<?= htmlspecialchars(date('Y-m-d\TH:i', strtotime($learning['post_date'] ?? '')), ENT_QUOTES, 'UTF-8'); ?>"
+          value="<?= h(date('Y-m-d\TH:i', strtotime($learning['post_date'] ?? ''))); ?>"
           class="border border-gray-300 w-full mb-4 p-2 rounded focus:outline-none focus:ring-2 focus:ring-blue-300">
+
+        <label for="study_minutes" class="block mb-2 text-sm font-medium text-gray-900">学習時間</label>
+         <input type="number" name="study_minutes" id="study_minutes" value="<?=  h($learning['study_minutes']); ?>"
+        class="border border-gray-300 w-full mb-4 p-2 rounded focus:outline-none focus:ring-2 focus:ring-green-400">
 
         <label for="category" class="block mb-2 text-sm font-medium text-gray-900">カテゴリー</label>  
         <input type="text" id="category" name="category" 
-          value="<?= htmlspecialchars($learning['category'], ENT_QUOTES, 'UTF-8'); ?>" 
+          value="<?= h($learning['category']); ?>" 
           class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 mb-4 focus:outline-none focus:ring-2 focus:ring-blue-300">
 
         <label for="comment" class="block mb-2 text-sm font-medium text-gray-900">コメント</label>
